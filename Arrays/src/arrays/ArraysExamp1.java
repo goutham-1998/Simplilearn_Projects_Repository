b99b17031453d03cd87cd7f303437fe26f0d8f07
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-02-25 18:48:12 +0530

    Changes have been comitted

---
+++ Arrays/src/arrays/ArraysExamp1.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class ArraysExamp1 {
+
+	public static void main(String[] args) {
+		int a[] = {10,20,30,40,50};
+		
+		System.out.println("Example for Single Dimensional Array");
+		
+		System.out.println("\nThe following array consists of elements: ");
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
---
+++ Arrays/src/arrays/ArraysExamp2.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class ArraysExamp2 {
+	public static void main(String[] args) {
+		
+		int a[][]= {{10,20,30,40},{50,60,70}};
+		
+		System.out.println("\nLength of row 1: " + a[0].length);
+		System.out.println("\nLength of row 2: " + a[1].length);
+
+}
+
+}
