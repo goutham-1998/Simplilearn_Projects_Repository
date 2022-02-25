130aa6af6be976e1a7433b176f4e4afdc5b3063e
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-02-25 18:08:25 +0530

    Changes have been comitted

---
+++ Strings/src/strings/StringExamps.java
@@ -0,0 +1,90 @@
+package strings;
+
+public class StringExamps {
+
+	public static void main(String[] args) {
+		//methods of strings
+				System.out.println("Demo of implementation of strings");
+				
+				String sl=new String("Hello World!!!");
+				System.out.println(sl.length());
+
+				//substring
+				String sub=new String("Welcome");
+				System.out.println(sub.substring(3));
+
+				//String Comparison
+				String s1="Hello World!!!";
+				String s2="Hello";
+				System.out.println(s1.compareTo(s2));
+
+				//IsEmpty
+				String s4="";
+				System.out.println(s4.isEmpty());
+
+				//toLowerCase
+				String s5="Hello";
+				System.out.println(s1.toLowerCase());
+				
+				//replace
+				String s6="Heldo";
+				String replace=s2.replace('d', 'l');
+				System.out.println(replace);
+
+				//equals
+				String x="Hello World!!!";
+				String y="Hello World";
+				System.out.println(x.equals(y));
+		 
+				System.out.println("\n");
+				System.out.println("Creating StringBuffer");
+				//Creating StringBuffer and append method
+				StringBuffer s=new StringBuffer("Hello World!!!");
+				s.append("I am doing great.");
+				System.out.println(s);
+
+				//insert method
+				s.insert(0, 'H');
+				System.out.println(s);
+
+				//replace method
+				StringBuffer sb=new StringBuffer("Hello");
+				sb.replace(0, 2, "hEl");
+				System.out.println(sb);
+
+				//delete method
+				sb.delete(0, 1);
+				System.out.println(sb);
+				
+				//StringBuilder
+				System.out.println("\n");
+				System.out.println("Creating StringBuilder");
+				StringBuilder sb1=new StringBuilder("Happy");
+				sb1.append("Learning");
+				System.out.println(sb1);
+
+				System.out.println(sb1.delete(0, 1));
+
+				System.out.println(sb1.insert(1, "Welcome"));
+
+				System.out.println(sb1.reverse());
+						
+				//conversion	
+				System.out.println("\n");
+				System.out.println("Conversion of Strings to StringBuffer and StringBuilder");
+				
+				String str = "Hello"; 
+		        
+		        // conversion from String object to StringBuffer 
+		        StringBuffer sbr = new StringBuffer(str); 
+		        sbr.reverse(); 
+		        System.out.println("String to StringBuffer");
+		        System.out.println(sbr); 
+		          
+		        // conversion from String object to StringBuilder 
+		        StringBuilder sbl = new StringBuilder(str); 
+		        sbl.append("world"); 
+		        System.out.println("String to StringBuilder");
+		        System.out.println(sbl);              		
+			}
+		}
