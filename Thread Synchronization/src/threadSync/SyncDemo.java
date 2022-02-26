c200992597bcdfb4c8f0d3a21b518627e4b1b1aa
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-02-26 16:27:47 +0530

    Chnages have been comitted

--- Thread Synchronization/src/threadSync/SyncDemo.java
+++ Thread Synchronization/src/threadSync/SyncDemo.java
@@ -43,10 +43,9 @@ public class SyncDemo {
 
 	public static void main(String[] args) {
 		Sender snd = new Sender(); 
-        ThreadedSend S1 = 
-            new ThreadedSend( " Hi " , snd ); 
-        ThreadedSend S2 = 
-            new ThreadedSend( " Bye " , snd ); 
+        ThreadedSend S1 = new ThreadedSend( " Hi " , snd );
+        ThreadedSend S2 = new ThreadedSend( " Bye " , snd );
+             
         S1.start(); 
         S2.start(); 
         try
