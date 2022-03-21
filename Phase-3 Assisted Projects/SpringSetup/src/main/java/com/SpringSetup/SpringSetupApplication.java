3b447ce28d628d964732f6e67e902bc3fba393d9
Goutham Gummadipudi <ggummadipudi@gmail.com>
2022-03-21 15:45:23 +0530

    Changes Added

---
+++ Phase-3 Assisted Projects/SpringSetup/src/main/java/com/SpringSetup/SpringSetupApplication.java
@@ -0,0 +1,13 @@
+package com.SpringSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSetupApplication.class, args);
+	}
+
+}
---
+++ Phase-3 Assisted Projects/SpringSetup/src/test/java/com/SpringSetup/SpringSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
